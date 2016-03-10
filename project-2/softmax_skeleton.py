976b29c1e2cf7cf38731bdac476b17de48462d72
Birkan Uzun <birkanuzun27@gmail.com>
2016-03-10 18:23:58 -0500

    Add Project 2

---
+++ project-2/softmax_skeleton.py
@@ -0,0 +1,45 @@
+from utils import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+def augmentFeatureVector(X):
+    columnOfOnes = np.zeros([len(X), 1]) + 1
+    return np.hstack((columnOfOnes, X))
+
+def computeProbabilities(X, theta):
+    #YOUR CODE HERE
+    pass
+
+def computeCostFunction(X, Y, theta, lambdaFactor):
+    #YOUR CODE HERE
+    pass
+
+def runGradientDescentIteration(X, Y, theta, alpha, lambdaFactor):
+    #YOUR CODE HERE
+    pass
+
+def softmaxRegression(X, Y, alpha, lambdaFactor, k, numIterations):
+    X = augmentFeatureVector(X)
+    theta = np.zeros([k, X.shape[1]])
+    costFunctionProgression = []
+    for i in range(numIterations):
+        costFunctionProgression.append(computeCostFunction(X, Y, theta, lambdaFactor))
+        theta = runGradientDescentIteration(X, Y, theta, alpha, lambdaFactor)
+    return theta, costFunctionProgression
+    
+def getClassification(X, theta):
+    X = augmentFeatureVector(X)
+    probabilities = computeProbabilities(X, theta)
+    return np.argmax(probabilities, axis = 0)
+
+def plotCostFunctionOverTime(costFunctionHistory):
+    plt.plot(range(len(costFunctionHistory)), costFunctionHistory)
+    plt.ylabel('Cost Function')
+    plt.xlabel('Iteration number')
+    plt.show()
+
+
+def computeTestError(X, Y, theta):
+    errorCount = 0.
+    assignedLabels = getClassification(X, theta)
+    return 1 - np.mean(assignedLabels == Y)
